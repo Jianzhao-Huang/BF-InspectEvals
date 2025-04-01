f2281d9c9e1c79094a81810acfbe59d8643e49df
jianzhao <jianzhao_huang@foxmail.com>
2025-04-01 11:31:19 +0800

    first commit

---
+++ benchflow_interface.py
@@ -0,0 +1,56 @@
+import json
+import os
+from typing import Any, Dict
+
+from datasets import load_dataset
+
+from benchflow import BaseBench
+from benchflow.schemas import BenchArgs, BenchmarkResult
+from inspect_ai.log import list_eval_logs, read_eval_log
+
+
+class InspectEvalBench(BaseBench):
+    def get_args(self, task_id: str) -> BenchArgs:
+        return BenchArgs(None)
+
+    def get_image_name(self) -> str:
+        return "huangjianzhao/benchflow:inspect_evals"
+
+    def get_results_dir_in_container(self) -> str:
+        return "/app/logs"
+
+    def get_log_files_dir_in_container(self) -> str:
+        return "/app/logs" # Useless
+
+    def get_result(self, task_id: str) -> BenchmarkResult:
+        try:
+            for eval_log in list_eval_logs(self.get_results_dir_in_container()):
+                for sample in read_eval_log(eval_log.name).samples:
+                    if sample.id == (task_id + 1):
+                        return BenchmarkResult(
+                            task_id=task_id,
+                            is_resolved=True,
+                            metrics={"score": sample.score},
+                            log=sample.messages,
+                            other={},
+                        )
+            else:
+                raise Exception(f"No eval log found for task_id: {task_id}")
+        
+        except Exception as e:
+            return BenchmarkResult(
+                task_id=task_id,
+                is_resolved=False,
+                metrics={"score": 0},
+                log={"error": str(e)},
+                other={"error": str(e)},
+            )
+
+        
+    def get_all_tasks(self, split: str) -> Dict[str, Any]:
+        self.run_bench(0, "", {})
+        eval_log = read_eval_log(list_eval_logs(self.get_results_dir_in_container())[0].name)
+        return {"task_ids": list(range(len(eval_log.eval.dataset.samples))), "error_message": ""}
+    
+    def cleanup(self):
+        pass
